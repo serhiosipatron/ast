b5b381e30c8aa9107366397ccf43c563aa2e0508
Sergey Sipatrin <@webinerds.com>
2019-09-20 14:10:46 +0300

    Main page title

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,16 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Тормозные колодки ast</h1>
+                <p>Производство дисковых Тормозных Колодок в Украине.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>О нас</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Ассортимент</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Инструкция</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Контакты</a></li>
+                <li><a href="javascript:" onClick={() => {props.onOpenArticle('intro')}}>О нас</a></li>
+                <li><a href="javascript:" onClick={() => {props.onOpenArticle('work')}}>Ассортимент</a></li>
+                <li><a href="javascript:" onClick={() => {props.onOpenArticle('about')}}>Инструкция</a></li>
+                <li><a href="javascript:" onClick={() => {props.onOpenArticle('contact')}}>Контакты</a></li>
             </ul>
         </nav>
     </header>
